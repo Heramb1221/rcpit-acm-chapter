5d1040af76d39d244dd8db71abdc024282096d89
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-09-17 23:46:05 +0530

    Footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React from "react";
+import footer from '@/assets/footer.png'
+import Image from "next/image";
+
+const Footer = () => {
+
+  return (
+    <div>
+        <footer className="bg-gradient-to-b from-blue-50 via-transparent py-10 sm:py-12 px-4 sm:px-6 lg:px-8">
+            <div className="max-w-9xl mx-auto">
+                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-4 sm:gap-8">
+                    <div className="col-span-2 sm:col-span-2 md:col-span-1 gap-6 sm:gap-8">
+                        <Image src={footer} alt="ACM Logo" className="sm:pb-6 md:pb-8 lg:pb-10" />
+                        <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error facere atque molestias vero eum voluptates, reprehenderit nostrum fuga quis modi.</p>
+                    </div>
+                    <div className="justify-center">
+                        <h3 className="text-base text-blue-400 uppercase sm:text-lg font-semibold mb-3 sm:mb-4">Membership</h3>
+                        <ul className="space-y-1 sm:space-y-2 text-sm sm:text-base">
+                            <li><a href="/events" className="text-gray-800 hover:text-blue-600">Our Events</a></li>
+                            <li><a href="/team" className="text-gray-800 hover:text-blue-600">Our Team</a></li>
+                            <li><a href="https://dl.acm.org/" target="_blank" className="text-gray-800 hover:text-blue-600">ACM Digital Library</a></li>
+                            <li><a href="https://www.acm.org/membership/membership-benefits" target="_blank" className="text-gray-800 hover:text-blue-600">Membership Benefits</a></li>
+                        </ul>
+                    </div>
+                    <div className="items-center justify-between">
+                        <h3 className="text-base text-blue-400 uppercase sm:text-lg font-semibold mb-3 sm:mb-4">Official Resources</h3>
+                        <ul className="space-y-1 sm:space-y-2 text-sm sm:text-base">
+                            <li><a href="https://learning.acm.org/" className="text-gray-800 hover:text-blue-600">ACM Learning Center</a></li>
+                            <li><a href="https://www.acm.org/code-of-ethics" className="text-gray-800 hover:text-blue-600">ACM Code of Ethics</a></li>
+                            <li><a href="https://www.acm.org/publications" target="_blank" className="text-gray-800 hover:text-blue-600">ACM Publications</a></li>
+                            <li><a href="https://awards.acm.org/" target="_blank" className="text-gray-800 hover:text-blue-600">ACM Awards</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className="text-base text-blue-400 sm:text-lg font-semibold mb-3 sm:mb-4">Lets Connect with each other</h3>
+                        <p className="text-gray-500 text-xs">Feel free to reach out for any inquiries, partnerships, or support—we’re here to help!</p>
+                        <div className="pt-6">
+                            <div className="flex flex-wrap " bis_skin_checked="1">
+                                <div className="p-2 w-full " bis_skin_checked="1">
+                                    <div className="h-full min-w-fit flex items-center border-gray-200 border p-4 rounded-lg hover:bg-blue-50" bis_skin_checked="1">
+                                    <img alt="mial" className=" w-8 h-8 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4 hidden md:block" src="https://cdn-icons-png.freepik.com/512/3870/3870795.png?ga=GA1.1.737403525.1733342298"/>
+                                    <div className="flex-grow" bis_skin_checked="1">
+                                        <a href="mailto:example@gmail.com" className="text-gray-900 title-font font-medium underline text-xs">example@gmail.com</a>
+                                        <p className="text-gray-500 text-xs">Team ACM, RCPIT, Shirpur</p>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <div>
+            <p className="text-sm px-8 p-4">Copyright © <span className="text-blue-400"><a href="https://www.rcpit.ac.in/">RCPIT, Shirpur</a></span>. All Rights reserved. <span className="text-xs text-gray-300 items-center">Developed by <a href="https://github.com/Heramb1221">Heramb Chaudhari</a></span></p>
+        </div>
+    </div>
+  );
+};
+
+export default Footer;
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,10 +26,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <Navbar />
-          {children}
-        </body>
-      </html>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
+        <Navbar />
+        <main className="flex-grow">{children}</main>
+        <Footer />
+      </body>
+    </html>
   );
 }
